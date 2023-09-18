ab531144a080302fabda481689791f6896a045a4
octaruggieri <oruggieri@ipem206fernandofader.edu.ar>
2023-09-18 15:45:04 -0300

    2

---
+++ main.js
@@ -0,0 +1,21 @@
+const typed = new Typed('.typed', {
+	strings: [
+		'<i class="pieza">Tu guía transformador de procesos.</i>',
+		'<i class="pieza">Tu mentor en la evolución de tus flujos de trabajo.</i>',
+		'<i class="pieza">Tu socio en la revolución de tu gestión.</i>',
+		'<i class="pieza">El catalizador de la mejora organizativa.</i>'
+	],
+
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
